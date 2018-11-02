2b485aa3d0870297489d1ff47ae91a0cf16d10d3
Alexanderzu <artminor@yandex.ru>
2018-11-02 19:01:56 +0300

    dz3-finish

--- src/index.js
+++ src/index.js
@@ -16,8 +16,28 @@
    isAllTrue([1, 2, 3, 4, 5], n => n < 10) // вернет true
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
+
+var fn = function () {
+};
 function isAllTrue(array, fn) {
+  if ( !(Array.isArray(array)) || array.length <= 0 ) {
+    throw new Error("empty array");
+  }
+  if ( typeof fn != 'function' ) {
+    throw new Error("fn is not a function");
+  } else {
+    for ( var i = 0; i < array.length; i++ ) {
+      var result = fn(array[i]);
+      if ( result == false ) {
+        return false;
+      }
+    }
+    if ( result == true ) {
+      return true;
+    }
+  }
 }
+isAllTrue([1,2], fn);
 
 /*
  Задание 2:
@@ -36,21 +56,63 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
 function isSomeTrue(array, fn) {
+  if ( !(Array.isArray(array)) || array.length <= 0 ) {
+    throw new Error("empty array");
+  }
+  if ( typeof fn != 'function' ) {
+    throw new Error("fn is not a function");
+  } else {
+    for ( var i = 0; i < array.length; i++ ) {
+      var result = fn(array[i]);
+      if ( result == true ) {
+        return true;
+      }
+    }
+    if ( result == false ) {
+      return false;
+    } 
+  }
 }
+isSomeTrue([1,2], fn);
 
 /*
  Задание 3:
 
- 3.1: Функция принимает заранее неизветсное количество аргументов, первым из которых является функция fn
- Функция должна поочередно запустить fn для каждого переданного аргумента (кроме самой fn)
+ 3.1: Функция принимает заранее неизветсное количество 
+ аргументов, первым из которых является функция fn.
+ Функция должна поочередно запустить fn для каждого 
+ переданного аргумента (кроме самой fn)
 
  3.2: Функция должна вернуть массив аргументов, для которых fn выбросила исключение
 
  3.3: Необходимо выбрасывать исключение в случаях:
    - fn не является функцией (с текстом "fn is not a function")
  */
-function returnBadArguments(fn) {
+
+var fn = function () {
+};
+function returnBadArguments(fn, ...args ) {
+
+  if ( typeof fn != 'function' ) {
+    throw new Error("fn is not a function");
+  }
+
+  var arr = [];
+
+  for ( var i = 0; i < args.length; i++ ) {
+
+    try {
+      fn(args[i]);
+    } catch(e) {
+     arr.push(args[i]);
+    }
+
+  }
+  return arr;
+
 }
+
+returnBadArguments(fn);
 
 /*
  Задание 4:
@@ -60,8 +122,10 @@ function returnBadArguments(fn) {
  4.2: Функция должна вернуть объект, у которого должно быть несколько методов:
    - sum - складывает number с переданными аргументами
    - dif - вычитает из number переданные аргументы
-   - div - делит number на первый аргумент. Результат делится на следующий аргумент (если передан) и так далее
-   - mul - умножает number на первый аргумент. Результат умножается на следующий аргумент (если передан) и так далее
+   - div - делит number на первый аргумент. Результат делится 
+   на следующий аргумент (если передан) и так далее
+   - mul - умножает number на первый аргумент. 
+   Результат умножается на следующий аргумент (если передан) и так далее
 
  Количество передаваемых в методы аргументов заранее неизвестно
 
@@ -69,8 +133,60 @@ function returnBadArguments(fn) {
    - number не является числом (с текстом "number is not a number")
    - какой-либо из аргументов div является нулем (с текстом "division by 0")
  */
-function calculator() {
+
+function calculator( number = 0 ) {
+
+  if ( !isFinite(number) ) {
+    throw new Error("number is not a number");
+  }
+  const calc = {
+    
+		sum: function() {
+      var result = number;
+      for ( var i = 0; i < arguments.length; i++ ) {
+        result += arguments[i];
+      }
+      return result;
+    },
+    dif: function() {
+      var result = number;
+      for ( var i = 0; i < arguments.length; i++ ) {
+        result -= arguments[i];
+      }
+      return result;
+    },
+    div: function() {
+      var result = number;
+      for ( var i = 0; i < arguments.length; i++ ) {
+        if ( arguments[i] == 0 ) {
+          throw new Error("division by 0");
+        } else {
+          result /= arguments[i];
+        }
+      }
+      return result;
+    },
+    mul: function() {
+      var result = number;
+      for ( var i = 0; i < arguments.length; i++ ) {
+        result *= arguments[i];
+      }
+      return result;
+    }
+  }
+  return calc;
 }
+
+const calc = calculator(0);
+const sumResult = calc.sum(1,2,3);
+const difResult = calc.dif(4,2);
+const divResult = calc.div(66,2);
+const mulResult = calc.mul(100,100);
+
+console.log(sumResult);
+console.log(difResult);
+console.log(divResult);
+console.log(mulResult);
 
 /* При решении задач, пострайтесь использовать отладчик */
 
