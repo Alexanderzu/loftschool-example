f0b9549feb0369318f674c29e0e686272ca1caf7
Alexanderzu <artminor@yandex.ru>
2018-11-13 18:30:37 +0300

    dz6

---
+++ src/index.js
@@ -0,0 +1,51 @@
+import { resolve } from "path";
+import { rejects } from "assert";
+
+/* ДЗ 6 - Асинхронность и работа с сетью */
+
+/*
+ Задание 1:
+
+ Функция должна возвращать Promise, который должен быть разрешен 
+ через указанное количество секунду
+
+ Пример:
+   delayPromise(3) // вернет promise, который будет разрешен через 3 секунды
+ */
+function delayPromise(seconds) {
+  // var promise = new Promise((resolve, rejects) => {
+  //   setTimeout(() => {
+  //     resolve("result")
+  //   }, seconds)
+  // });
+  // promise.
+  //   then(
+  //     result => {
+  //       console.log("Fulfilled: " + result);
+  //     }
+  //   );
+}
+// delayPromise(10000)
+
+/*
+ Задание 2:
+
+ 2.1: Функция должна вернуть Promise, 
+ который должен быть разрешен с массивом городов в качестве значения
+
+ Массив городов пожно получить отправив асинхронный запрос по адресу
+ https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
+
+ 2.2: Элементы полученного массива должны быть отсортированы по имени города
+
+ Пример:
+   loadAndSortTowns().then(towns => console.log(towns)) 
+   // должна вывести в консоль отсортированный массив городов
+ */
+function loadAndSortTowns() {
+}
+
+export {
+    delayPromise,
+    loadAndSortTowns
+};
